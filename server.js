fce82e11975a842bf510cba740b1b4738547d9f3
Aditya Jha <adityaiitr@hotmail.com>
2017-02-13 19:26:06 +0530

    redirected all urls to home url / to handle refresh

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ let app = express();
 
 app.use(express.static('public'));
 
+// redirect all urls to /
+app.use(function (req, res, next) {
+    res.redirect('/');
+});
+
 app.listen(3000, function () {
    console.log("listening on http://localhost:3000");
 });
