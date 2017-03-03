a8273a52d9dd31676a1ee594ea6f4c88348430d9
Aditya Jha <adityaiitr@hotmail.com>
2017-03-03 10:38:24 +0530

    removed add question from paper element on dashboard page

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -3,7 +3,6 @@
 import React, {PropTypes} from "react";
 import Paper from "material-ui/Paper";
 import RaisedButton from 'material-ui/RaisedButton';
-import Divider from 'material-ui/Divider';
 import {Grid, Row, Col} from "react-flexbox-grid";
 import SummaryCardComponent from "./SummaryCardComponent";
 import {browserHistory} from "react-router";
@@ -37,11 +36,14 @@ export default class DashboardComponent extends React.Component {
                         <SummaryCardComponent title="Questions Added" value={10} />
                     </Col>
                 </Row>
+                <Row center="xs">
+                    <Col xs={12}>
+                        <RaisedButton label="Add Question" primary={true} onClick={this.addQuestionButton.bind(this)} />
+                    </Col>
+                </Row>
                 <Row>
                     <Col xs={12}>
                         <Paper zDepth={1} rounded={false}>
-                            <RaisedButton label="Add Question" primary={true} onClick={this.addQuestionButton.bind(this)} />
-                            <Divider />
                             <h4>Last Added Question</h4>
                         </Paper>
                     </Col>
