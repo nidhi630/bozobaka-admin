3ee853c62579535a55a943cc21102ba7889be9a1
Aditya Jha <aditya.jha@indifi.com>
2017-04-08 16:15:23 +0530

    eslint fixes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 "use strict";
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 // create our app
 let app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // send all requests to index.html so browserHistory in React Router works
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(3000, function () {
-   console.log("listening on http://localhost:3000");
+    console.log("listening on http://localhost:3000");
 });
