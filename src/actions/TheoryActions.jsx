2e89d308dae57bc1bded5516980da49b0b224d2a
Aditya Jha <aditya.jha@indifi.com>
2017-04-14 12:12:52 +0530

    fixes according to eslint

--- src/actions/TheoryActions.jsx
+++ src/actions/TheoryActions.jsx
@@ -17,6 +17,97 @@ import {
 } from "./ActionConstants";
 import {updateTheory as updateTheoryRequest} from "./../services/TheoryService";
 
+export function theoryHasErrored(hasErrored, errorMessage) {
+    return {
+        type: THEORY_HAS_ERRORED,
+        hasErrored,
+        errorMessage
+    };
+}
+export function theoryIsLoading(isLoading) {
+    return {
+        isLoading,
+        type: THEORY_IS_LOADING
+    };
+}
+
+export function theoryRequestSuccess(requestSuccess) {
+    return {
+        type: THEORY_REQUEST_SUCCESS,
+        requestSuccess
+    };
+}
+
+export function theoryUpdateSection(sectionId) {
+    return {
+        type: THEORY_UPDATE_SECTION,
+        sectionId
+    };
+}
+
+export function theoryUpdateSource(sourceId) {
+    return {
+        type: THEORY_UPDATE_SOURCE,
+        sourceId
+    };
+}
+
+export function theoryUpdateHeading(heading) {
+    return {
+        type: THEORY_UPDATE_HEADING,
+        heading
+    };
+}
+
+export function theoryUpdateTheory(theory) {
+    return {
+        type: THEORY_UPDATE_THEORY,
+        theory
+    };
+}
+
+export function theoryUpdateL1(l1Id) {
+    return {
+        type: THEORY_UPDATE_L1,
+        l1Id
+    };
+}
+
+export function theoryUpdateL2(l2Id) {
+    return {
+        type: THEORY_UPDATE_L2,
+        l2Id
+    };
+}
+
+export function theoryUpdateL3(l3Id) {
+    return {
+        type: THEORY_UPDATE_L3,
+        l3Id
+    };
+}
+
+export function theoryUpdateL4(l4Id) {
+    return {
+        type: THEORY_UPDATE_L4,
+        l4Id
+    };
+}
+
+export function theoryUpdateStatus(status) {
+    return {
+        type: THEORY_UPDATE_STATUS,
+        status
+    };
+}
+
+export function theoryUpdateParsedTheory(parsedTheory) {
+    return {
+        type: THEORY_UPDATE_PARSED_THEORY,
+        parsedTheory
+    };
+}
+
 export function fetchTheory() {
 
 }
@@ -26,109 +117,18 @@ export function postTheory() {
         dispatch(theoryIsLoading(true));
 
         /* TODO: validate data before post */
-        const data = getState.theory.newTheory;
+        const data = getState.newTheory;
         console.log(data);
         updateTheoryRequest({
-            method: "post",
+            method: data.id ? "put" : "post",
             data
-        }).then((res) => {
+        }).then(() => {
             dispatch(theoryIsLoading(false));
             dispatch(theoryRequestSuccess(true));
         }).catch((err) => {
             dispatch(theoryIsLoading(false));
             dispatch(theoryRequestSuccess(false));
             dispatch(theoryHasErrored(true, err.message));
-        })
-    }
-}
-
-export function theoryHasErrored(hasErrored, errorMessage) {
-    return {
-        type: THEORY_HAS_ERRORED,
-        hasErrored,
-        errorMessage
-    }
-}
-export function theoryIsLoading(isLoading) {
-    return {
-        isLoading,
-        type: THEORY_IS_LOADING
-    }
-}
-
-export function theoryRequestSuccess(requestSuccess) {
-    return {
-        type: THEORY_REQUEST_SUCCESS,
-        requestSuccess
-    }
-}
-
-export function theoryUpdateSection(sectionId) {
-    return {
-        type: THEORY_UPDATE_SECTION,
-        sectionId
-    }
-}
-
-export function theoryUpdateSource(sourceId) {
-    return {
-        type: THEORY_UPDATE_SOURCE,
-        sourceId
-    }
-}
-
-export function theoryUpdateHeading(heading) {
-    return {
-        type: THEORY_UPDATE_HEADING,
-        heading
-    }
-}
-
-export function theoryUpdateTheory(theory) {
-    return {
-        type: THEORY_UPDATE_THEORY,
-        theory
-    }
-}
-
-export function theoryUpdateL1(l1Id) {
-    return {
-        type: THEORY_UPDATE_L1,
-        l1Id
-    }
-}
-
-export function theoryUpdateL2(l2Id) {
-    return {
-        type: THEORY_UPDATE_L2,
-        l2Id
-    }
-}
-
-export function theoryUpdateL3(l3Id) {
-    return {
-        type: THEORY_UPDATE_L3,
-        l3Id
-    }
-}
-
-export function theoryUpdateL4(l4Id) {
-    return {
-        type: THEORY_UPDATE_L4,
-        l4Id
-    }
-}
-
-export function theoryUpdateStatus(status) {
-    return {
-        type: THEORY_UPDATE_STATUS,
-        status
-    }
-}
-
-export function theoryUpdateParsedTheory(parsedTheory) {
-    return {
-        type: THEORY_UPDATE_PARSED_THEORY,
-        parsedTheory
-    }
+        });
+    };
 }
