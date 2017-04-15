6ee8b9e49da78a571e83fd5c34fd142574a82242
Aditya Jha <aditya.jha@indifi.com>
2017-04-15 12:22:29 +0530

    added filter reducer

---
+++ src/actions/FilterActions.jsx
@@ -0,0 +1,133 @@
+"use strict";
+
+import {
+    FILTER_MAX_DIFFICULTY,
+    FILTER_MIN_DIFFICULTY,
+    FILTER_ID,
+    FILTER_HEADING,
+    FILTER_QUESTION,
+    FILTER_STATUS,
+    FILTER_L1,
+    FILTER_L2,
+    FILTER_SOURCE,
+    FILTER_SECTION
+} from "./../actions/ActionConstants";
+
+export function setId(id) {
+    return {
+        type: FILTER_ID,
+        id
+    };
+}
+
+export function setMinDifficulty(minDifficulty) {
+    return {
+        type: FILTER_MIN_DIFFICULTY,
+        minDifficulty
+    };
+}
+
+export function setMaxDifficulty(maxDifficulty) {
+    return {
+        type: FILTER_MAX_DIFFICULTY,
+        maxDifficulty
+    };
+}
+
+export function setHeading(heading) {
+    return {
+        type: FILTER_HEADING,
+        heading
+    };
+}
+
+export function setQuestion(question) {
+    return {
+        type: FILTER_QUESTION,
+        question
+    };
+}
+
+export function setStatus(status) {
+    return {
+        type: FILTER_STATUS,
+        status
+    };
+}
+
+export function setSection(sectionId) {
+    return {
+        type: FILTER_SECTION,
+        sectionId
+    };
+}
+
+export function setL1(l1Id) {
+    return {
+        type: FILTER_L1,
+        l1Id
+    };
+}
+
+export function setL2(l2Id) {
+    return {
+        type: FILTER_L2,
+        l2Id
+    };
+}
+
+export function setSource(source) {
+    return {
+        type: FILTER_SOURCE,
+        source
+    };
+}
+
+function getCommonFilters(state) {
+    /* TODO: add source support */
+    const {id, status, sectionId, l1Id, l2Id} = state.filters;
+    const filter = {};
+    if (!id) {
+        filter.id = {ilike: id};
+    }
+    if (!status) {
+        filter.status = status;
+    }
+    if (!sectionId) {
+        filter.sectionId = sectionId;
+    }
+    if (!l1Id) {
+        filter.l1Id = l1Id;
+    }
+    if (!l2Id) {
+        filter.l1Id = l2Id;
+    }
+    return filter;
+}
+
+export function getQuestionFilter(state) {
+    const {question, minDifficulty, maxDifficulty} = state.filters;
+    const filter = getCommonFilters(state);
+
+    filter.difficulty = {
+        and: [{gte: minDifficulty}, {lte: maxDifficulty}]
+    };
+
+    if (!question) {
+        filter.question = {ilike: question};
+    }
+
+    return filter;
+}
+
+export function getTheoryFilter(state) {
+    const {theory} = state.filters;
+    const filter = getCommonFilters(state);
+
+    if (!theory) {
+        filter.theory = {ilike: theory};
+    }
+
+    return filter;
+}
+
--- src/reducers/FiltersReducer.jsx
+++ src/reducers/FiltersReducer.jsx
@@ -1,11 +1,80 @@
 "use strict";
 
-let defaultState = {
+import {
+    FILTER_MAX_DIFFICULTY,
+    FILTER_MIN_DIFFICULTY,
+    FILTER_ID,
+    FILTER_HEADING,
+    FILTER_QUESTION,
+    FILTER_STATUS,
+    FILTER_L1,
+    FILTER_L2,
+    FILTER_SOURCE,
+    FILTER_SECTION
+} from "./../actions/ActionConstants";
 
+let defaultState = {
+    id: "",
+    heading: "",
+    status: "",
+    sectionId: "",
+    l1Id: "",
+    l2Id: "",
+    source: "",
+    minDifficulty: 0,
+    maxDifficulty: 100
 };
 
 export function FilterReducer(state = defaultState, action) {
     switch (action.type) {
+        case FILTER_SECTION:
+            return {
+                ...state,
+                sectionId: action.sectionId
+            };
+        case FILTER_HEADING:
+            return {
+                ...state,
+                heading: action.heading
+            };
+        case FILTER_ID:
+            return {
+                ...state,
+                id: action.id
+            };
+        case FILTER_L1:
+            return {
+                ...state,
+                l1Id: action.l1Id
+            };
+        case FILTER_L2:
+            return {
+                ...state,
+                l2Id: action.l2Id
+            };
+        case FILTER_QUESTION:
+            return {
+                ...state,
+                question: action.question
+            };
+        case FILTER_MAX_DIFFICULTY:
+            return {
+                ...state,
+                maxDifficulty: action.maxDifficulty
+            };
+        case FILTER_MIN_DIFFICULTY:
+            return {
+                ...state,
+                minDifficulty: action.minDifficulty
+            };
+        case FILTER_STATUS:
+            return {
+                ...state,
+                status: action.status
+            };
+        case FILTER_SOURCE:
+            /* TODO: handle filter source case */
+            return state;
         default:
             return state;
     }
